d42e7d4e86999ab7ac2b6e42c2cb03526080b285
howieh123 <hhalberstein@gmail.com>
2016-05-16 10:42:48 -0400

    added split.py python version of UNIX split command

---
+++ split.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 16:50:38 2016
+swiped for the most part from http://stackoverflow.com/questions/22751000/split-large-text-filearound-50gb-into-multiple-files
+just added command line parsing
+@author: howieh123
+"""
+from itertools import chain, islice
+import argparse
+
+def chunks(filepart, nlines):
+   iterable = iter(filepart)
+   while True:
+       yield chain([next(iterable)], islice(iterable, nlines-1))
+
+#Command Line parsing to get filename and number of lines
+parser = argparse.ArgumentParser(description='Simple Python Split Program, splits large files into parts filename.0, filename.1, etc')
+parser.add_argument('-f','--filename', help='Name of File to split', required=True)
+parser.add_argument('-l','--lines', type=int, help='Number of Lines to split the file at', required=True)
+
+args = vars(parser.parse_args())
+
+numoflines = args['lines']
+filename = args['filename']
+
+with open(filename) as bigfile:
+    for part, lines in enumerate(chunks(bigfile, numoflines)):
+        file_split = '{}.{}'.format(filename,part)
+        with open(file_split, 'w') as f:
+            f.writelines(lines)
+
